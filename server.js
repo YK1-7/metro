f643817a0975233dfc08d314c4ab64a43f84bbbe
YK1-7 <153370544+YK1-7@users.noreply.github.com>
2025-04-21 20:37:47 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+  app.use("/auth", require("./routes/auth"));
+
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
